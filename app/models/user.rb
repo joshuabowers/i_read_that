d4d9d54bfe0f5d8c2cecc3c563286a9d0948d792
Joshua Bowers <joshua.bowers+code@gmail.com>
2014-05-17 23:17:44 -0700

    Small steps towards eventually supporting usernames.

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,10 @@ class User
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :username, :email, :password, :password_confirmation, :remember_me
 
   ## Database authenticatable
+  field :username,           type: String, default: ""
   field :email,              type: String, default: ""
   field :encrypted_password, type: String, default: ""
 
