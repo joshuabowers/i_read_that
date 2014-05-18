98776559be00904ef441bf5d004228024f0c44eb
Joshua Bowers <joshua.bowers+code@gmail.com>
2014-05-17 21:58:41 -0700

    Initial rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IReadThat::Application.config.secret_token = '5fae1ff919bb68f32be3fff6ec6aaa470855170788b39f66d8db010937a202c47b7c294a60971285af72b064879a687a0f2554bda7c45889977054e420d31bd5'
