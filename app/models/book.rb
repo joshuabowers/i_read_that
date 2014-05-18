55528d0b82a309b7ba88d2047e94f803f2895f43
Joshua Bowers <joshua.bowers+code@gmail.com>
2014-05-18 00:47:39 -0700

    Added some logic for creating new books for the app using google books info.

--- app/models/book.rb
+++ app/models/book.rb
@@ -10,6 +10,18 @@ class Book
     GoogleBooks::API.search(query, api_key: google_books_api_key)
   end
   
+  def self.create_from_google_books_search(query)
+    google_books_search(query).map do |book|
+      Book.find_or_create_by(
+        title: book.title,
+        authors: book.authors,
+        isbn: book.isbn,
+        pages: book.page_count,
+        covers: book.covers
+      )
+    end
+  end
+  
   def self.google_books_api_key
     @@api_key ||= ENV['GOOGLE_BOOKS_API_KEY']
   end
