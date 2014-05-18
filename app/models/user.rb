1c75237a24640af5b7c0cfdbf6d13a108984e0b4
Joshua Bowers <joshua.bowers+code@gmail.com>
2014-05-17 23:08:58 -0700

    Some slight modifications to user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User
   include Mongoid::Document
+  include Authority::UserAbilities
   rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
