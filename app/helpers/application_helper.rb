3b9f94d3842ec09491f7c56a842e44986b3a663b
Joshua Bowers <joshua.bowers+code@gmail.com>
2014-07-03 04:18:43 -0700

    Provided an alternate way to fall back if GBS fails to return a cover image.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def cover_for( book )
     covers = book.covers.with_indifferent_access
-    covers[:small] || covers[:thumbnail] || covers[:medium] || covers.values.first
+    covers[:small] || covers[:thumbnail] || covers[:medium] || "http://covers.openlibrary.orb/b/isbn/#{book.isbn}-S.jpg"
   end
 end
